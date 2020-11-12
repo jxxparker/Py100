0399005ebd9c6caa621fd1d52ba3d8040302a9ff
Jihun Park <jxxparker@gmail.com>
2020-11-11 18:27:27 -0600

    first

---
+++ Section1/Sec5/main.py
@@ -0,0 +1,23 @@
+# #section 5
+# print("Hello World") #printing
+
+# #section 6
+# print("Day 1 Python Print Function")
+# print("The function is declared like this")
+# print("print('what to print')")
+
+# #section 7 
+# print("Hello World! \nHello World")
+
+# #Section 8
+# print("Day 1 -String Manipulation")
+# print("String concatenation is done with the " + "+ sign")
+# print('e.g print("Hello " + " World)') 
+
+# #Section 9
+# # input("What is your name?")
+
+# print("Hello " + input("What is your name")) 
+
+#Section 10
+print( len(input("What is your name? ")))
