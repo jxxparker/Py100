2bf6f4135ea3c39b862bc1f3cd74ce8dd62ffee1
Jihun Park <jxxparker@gmail.com>
2020-12-22 12:00:40 -0600

    nested dict

---
+++ D9/91/main.py
@@ -0,0 +1,28 @@
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 1},
+    "Korea": {"cities_visited": ["Seoul", "Busan", "Jeju"], "total_visitgs": 5 }
+}
+
+["A", "B", "C", "D"]
+
+#Nesting dictionary in a list
+travel_log = [
+    {
+    "country": "France", 
+    "cities_visited": ["Paris", "Lille", "Dijon"], 
+    "total_visits": 1
+    },
+
+    {
+    "country": "Korea", 
+    "cities_visited": ["Seoul", "Busan", "Jeju"], 
+    "total_visitgs": 5 
+    }
+    
+]
+
