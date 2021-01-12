645d8c203bd8e788be6c1c6ff9d454e8b94d70b9
Jihun Park <jxxparker@gmail.com>
2021-01-12 13:40:21 -0600

    51

---
+++ D5/51/main.py
@@ -0,0 +1,10 @@
+# for number in range(1,10): #1-9
+#         print(number)
+
+# for number in range(1,10,3): #1,4,7
+#     print(number)
+
+total = 0
+for number in range(1,101): 
+    total += number #adds all the number from 1-100
+print(total)
