d3e54fff23e945ae0614727cde6302ec1b06d12a
Jihun Park <jxxparker@gmail.com>
2021-01-14 19:10:12 -0600

    doine

--- D7/73_74/main.py
+++ D7/73_74/main.py
@@ -91,10 +91,14 @@ while not end_of_game:
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
     if guess not in chosen_word:
-        print("No Match")
+        lives -= 1
+        if lives == 0:
+          end_of_game = True
+          print("You Lose!")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
+    
 
     #Check if user has got all letters.
     if "_" not in display:
@@ -102,3 +106,4 @@ while not end_of_game:
         print("You win.")
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
