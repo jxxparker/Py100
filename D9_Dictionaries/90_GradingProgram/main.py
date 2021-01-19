98b6580cc003a81b042d8af7e140d50b0e45c170
Jihun Park <jxxparker@gmail.com>
2021-01-19 15:19:27 -0600

    Changing

--- D9_Dictionaries/90_GradingProgram/main.py
+++ D9_Dictionaries/90_GradingProgram/main.py
@@ -5,12 +5,9 @@ student_scores = {
   "Draco": 74,
   "Neville": 62,
 }
-# 🚨 Don't change the code above 👆
 
-#TODO-1: Create an empty dictionary called student_grades.
 student_grades = {}
 
-#TODO-2: Write your code below to add the grades to student_grades.👇
 for student in student_scores:
     score = student_scores[student]
     if score > 90:  
@@ -22,9 +19,6 @@ for student in student_scores:
     else:
         student_grades[student] = "fail"
     
-    
-
-# 🚨 Don't change the code below 👇
 print(student_grades)
 
 
