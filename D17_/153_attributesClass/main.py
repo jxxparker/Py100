3034767bb5a79a77d8733558ac7e144417db00cf
Jihun Park <jxxparker@gmail.com>
2021-03-01 10:58:37 -0600

    classes

---
+++ D17_/153_attributesClass/main.py
@@ -0,0 +1,19 @@
+class User:
+
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+
+    
+
+user_1 = User("001", "jihun")
+user_2 = User("002","jamess")
+
+print(user_1.followers)
+
+
+
+
+
+
