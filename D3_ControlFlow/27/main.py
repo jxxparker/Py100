148f9eaffa0b46ed66578e82e96eee1b568037c7
Jihun Park <jxxparker@gmail.com>
2021-03-08 09:15:16 -0600

    if/else

--- D3_ControlFlow/27/main.py
+++ D3_ControlFlow/27/main.py
@@ -1,9 +1,7 @@
-
 print("welcome to the rollercoaster!")
-height = int(input("what is your height in cm?"))
-
+height = int(input("what is your height in cm? "))
 
 if height > 120:
-    print("Taller than 120, you are good")
-else:
-    print("not taller than 120, you can not")
+    print("you can ride the rollercoaster")
+else :
+    print("you can't ride the rollercoaster")
