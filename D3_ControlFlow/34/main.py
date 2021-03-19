1c1cee4ac1c1e06488a58877ed9d56027606084b
Jihun Park <jxxparker@gmail.com>
2021-03-19 14:13:55 -0500

    pushing

---
+++ D3_ControlFlow/34/main.py
@@ -0,0 +1,3 @@
+print("welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
--- D4_RandomFxn/39/main.py
+++ D4_RandomFxn/39/main.py
@@ -1,16 +1,7 @@
 import random
-# import my_module
-
-random.seed(321) #initiliazer
-
+import my_module
 
 random_integer = random.randint(1,10)
-print(random_integer)
+print(random_integer) #prints 6
 
-# print(my_module.pi)
-
-random_float = random.random() * 5 #always generate 0-1
-print(random_float)
-
-love_score = random.randint(1,100)
-print(f"your love score is {love_score}")
+print(my_module.pi) #prints 3.14
