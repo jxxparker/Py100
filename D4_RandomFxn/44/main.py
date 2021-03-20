c21ef27a9ad158da739f9d01d0507d1dbf971484
Jihun Park <jxxparker@gmail.com>
2021-03-20 15:20:36 -0500

    rows

--- D4_RandomFxn/44/main.py
+++ D4_RandomFxn/44/main.py
@@ -2,19 +2,20 @@ row1 = ["⬜️","⬜️","⬜️"]
 row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
 map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ") #lets say i put 32
+
+horizontal = int(position[0]) #horizintal becomes 3, since position[0] = 3
+print(horizontal) 
+verticle = int(position[1]) #same logic as horizintal but with verticle
+print(verticle) 
+
+selected_row = map[verticle - 1] #have to subtract 1 since for programming reasons 0,1,2
+selected_row[horizontal - 1] = "X" 
 
 print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
 
-horizontal = int(position[0])
-print(horizontal)
-verticle = int(position[1])
-print(verticle)
 
-selected_row = map[verticle - 1]
-selected_row[horizontal -1] = "X"
-
-print(f"{row1}\n{row2}\n{row3}")
 
 
 
