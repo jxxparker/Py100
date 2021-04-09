e9b9e1de9c88f2a07ecc819c347d55273e391bfc
Jihun Park <jxxparker@gmail.com>
2021-04-09 12:31:36 -0500

    printing 1-100

--- D5_Loops/51/main.py
+++ D5_Loops/51/main.py
@@ -1,10 +1,11 @@
-# for number in range(1,10): #1-9
-#         print(number)
+#for loop with range
+# for number in range(1, 11): #prints 1-10
+#     print(number)
 
-# for number in range(1,10,3): #1,4,7
+# for number in range(1, 11, 3): #prints 1, 4, 10 
 #     print(number)
 
 total = 0
-for number in range(1,101): 
-    total += number #adds all the number from 1-100
+for number in range(1, 101):
+    total += number #adds every number from 1 - 100
 print(total)
