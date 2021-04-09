f5e737fe2d38c11ce35b3c64f02760e2b4c940df
Jihun Park <jxxparker@gmail.com>
2021-04-09 12:27:16 -0500

    finished

--- D5_Loops/50/main.py
+++ D5_Loops/50/main.py
@@ -3,13 +3,13 @@ for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
 
-# highest_score = 0
-# for score in student_scores:
-#   if score > highest_score:
-#     print(score)
-#     highest_score = score
+highest_score = 0
+for score in student_scores:
+  if score > highest_score:
+    print(score)
+    highest_score = score
 
-# print(f"the highest score is: {highest_score}")
+print(f"the highest score is: {highest_score}")
 
-# #easy way
-# # print(max(student_scores))
+#easy way
+# print(max(student_scores))
