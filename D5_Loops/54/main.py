cb6c194497427b593c893c47e4508f0a9c56728a
Jihun Park <jxxparker@gmail.com>
2021-04-10 21:57:12 -0500

    change

--- D5_Loops/54/main.py
+++ D5_Loops/54/main.py
@@ -9,18 +9,10 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Eazy Level - Order not randomised:
-# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-password = ""
-for char in range(1, nr_letters + 1):
-    #1-4
-    password += random.choice(letters)
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+for char in range(1, nr_letters):
 
-for char in range(1, nr_symbols + 1):
-    password += random.choice(symbols)
 
-for char in range(1, nr_numbers + 1):
-    password += random.choice(numbers)
-
-print(password)
-
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
