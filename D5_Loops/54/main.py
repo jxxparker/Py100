36c044ca1d2d1e3f533dc89bf93c717642363e27
Jihun Park <jxxparker@gmail.com>
2021-04-19 13:21:48 -0500

    passwordgen

--- D5_Loops/54/main.py
+++ D5_Loops/54/main.py
@@ -9,10 +9,43 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-for char in range(1, nr_letters):
+#---------Eazy Level - Order not randomised:------------
 
+# #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+# password = "" #start with empty string
+
+# for char in range(1, nr_letters + 1):
+#     password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#     password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#     password += random.choice(numbers)
+
+# print(password)
+#--------------------------------------------
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list += random.choice(letters)
+
+for char in range(1, nr_symbols + 1):
+    password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers +1):
+    password_list += random.choice(numbers)
+
+print(password_list) #before randomzied
+random.shuffle(password_list)
+print(password_list) #after randomzied
+
+password = ""
+for char in password_list:
+    password += char
+    print(password) #showing each addition of the password
+print(f" Your password is : {password}")
