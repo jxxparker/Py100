9361ea79ba5ed346524682bf2ee4c13a6b4a8dfb
Jihun Park <jxxparker@gmail.com>
2021-04-26 10:44:26 -0500

    fxn 1+ inputs

--- D8_FxnParameter/80/main.py
+++ D8_FxnParameter/80/main.py
@@ -1,19 +1,8 @@
-def greet():
-    print("Hello")
-    print("Howdo you do")
-    print("Isn't the weather nice today")
-#greet()
-
-def greet_with_name(name):
-    print(f"hello {name}")
-    print(f"how doyou do {name}")
-
-# greet_with_name("jihun")
-
 #function with more than 1 input
+
 def greet_with(name, location):
     print(f"Hello {name}")
-    print(f"Location is {location}")
+    print(f"location is {location}")
 
-greet_with("james", "chicago")
-greet_with(location="london", name="mike")
+greet_with("jihun", "los angeles")
+greet_with(location="paris", name="jordan")
