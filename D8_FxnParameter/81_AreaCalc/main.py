9f2a9ee9d1f20687be1d9ff36c79674d2951ec98
Jihun Park <jxxparker@gmail.com>
2021-04-26 16:20:10 -0500

    prime

--- D8_FxnParameter/81_AreaCalc/main.py
+++ D8_FxnParameter/81_AreaCalc/main.py
@@ -2,9 +2,9 @@ def paint_calc(height, width, cover):
     cans = (height * width) / (cover)
     print(f"{cans} is the amount of cans needed")
 
-
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
+
 
---
+++ D8_FxnParameter/82_PrimeNumberChecker/forLoop.py
@@ -0,0 +1,18 @@
+# a = ["Banana", "apple", "microsoft"]
+# b = [20, 10, 5]
+# total = 0
+
+# # print(a[0])
+# # print(a[1])
+# # print(a[2])
+
+# # for element in a:
+# #     print(element)
+
+# for e in b:
+#     total = total + e
+# print(total)
+
+for i in range(2, 33):
+    if 33 % i == 0:
+        print(i)
