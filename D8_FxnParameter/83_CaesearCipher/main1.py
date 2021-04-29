a5854c4dc961fc6dd50fe818f6f76f79385575a4
Jihun Park <jxxparker@gmail.com>
2021-04-29 15:11:12 -0500

    p1 caesar

--- D8_FxnParameter/83_CaesearCipher/main1.py
+++ D8_FxnParameter/83_CaesearCipher/main1.py
@@ -7,7 +7,7 @@ shift = int(input("Type the shift number:\n"))
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
+    #e.g. fjasd;
     #plain_text = "hello"
     #shift = 5
     #cipher_text = "mjqqt"
