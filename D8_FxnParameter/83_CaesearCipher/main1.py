18637377b3acd40649832d414d8be38c5dca24cc
Jihun Park <jxxparker@gmail.com>
2021-05-03 22:44:54 -0500

    changes

--- D8_FxnParameter/83_CaesearCipher/main1.py
+++ D8_FxnParameter/83_CaesearCipher/main1.py
@@ -19,3 +19,7 @@ shift = int(input("Type the shift number:\n"))
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+
+
+
