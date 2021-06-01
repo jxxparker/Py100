92fbde50eed2da42e534a1d906368ed1e852fa62
Jihun Park <jxxparker@gmail.com>
2021-06-01 16:04:48 -0500

    pushed

--- Udacity/11_StringsAsSequence/main.py
+++ Udacity/11_StringsAsSequence/main.py
@@ -7,3 +7,36 @@
 #list
 #tuple
 #str
+
+# s = "SCHOOLED"
+# print(s[-4:])
+# print(s[1::2])
+# print(s[1] + s[3] + s[5] + s[7])
+# print(s[::2])
+# print(s[1] + s[3] + s[-3] + s[-1])
+# print(s[::5])
+
+# s1 = "Udacity"
+# print(s1[0:2]) #Ud
+# print(s1[4:7]) #ity
+# print(s1[1:5]) #daci
+
+# print(s[1:5:2]) #dc
+# print(s[4::-2]) #iaU #if you omit reversing
+# print(s[::-1]) #yticadU
+
+# sa = "Hello World"
+# print(sa[1])
+
+# print(sa[-2])
+
+# print(sa[-4:])
+# print(sa[::-1])
+# print(sa[::-4])
+
+s2 = "ham cheese bacon eggs"
+ingredients = s2.split()
+print(ingredients)
+print(ingredients[::2])
+ingredients[1:-1] = ["spinach"]
+print(ingredients)
---
+++ Udacity/12_Lists/main.py
@@ -0,0 +1,8 @@
+#Lists 
+#A Mutable sequence of heterogenous elements
+
+letters = ['a', 'b', 'c']
+both = [1, 2, 'a', 'b']
+numbers = [1, 2, 3]
+nested = [letters, numbers]
+
