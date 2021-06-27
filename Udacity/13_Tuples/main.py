13d84021c0a1b29dac6265fdf912e50578bb2358
Jihun Park <jxxparker@gmail.com>
2021-06-27 09:26:03 -0500

    pushing 13

---
+++ Udacity/13_Tuples/main.py
@@ -0,0 +1,17 @@
+pet = (1, 'cat')
+
+print(pet[0])
+print(pet[1])
+
+fish = (10 ,2 ,'red','blue')
+print(fish[0])
+
+#tuples are immutable, meaning it can't be changed
+print(fish[:3])
+#(10,2,'red)
+
+tuples = ([1,2,3],[4,5,6])
+print(tuples)
+tuples[0].append(4)
+print(tuples)
+
