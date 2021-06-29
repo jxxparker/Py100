79ce49be67e432077de1b7dd5534fa5841858e50
Jihun Park <jxxparker@gmail.com>
2021-06-29 15:23:42 -0500

    more

---
+++ Udacity/19_SetsandMappingQuizzes/main.py
@@ -0,0 +1,11 @@
+# {} = dict 
+# {'red': 'ff0000', 'green': '00ff00', 'blue': '0000ff'} = dict
+# {1, True, 'love'} = set
+# set() = set
+# dict(x=4, y=1) = dict
+
+EFFICIENT_LETTERS = set('BCDGIJLMNOPSUVWZ')
+
+def is_efficient(letters):
+   return set(letters)
+
