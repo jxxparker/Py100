fda817d06b23acf5d60f299c25f0747748cbefef
Jihun Park <jxxparker@gmail.com>
2021-06-29 11:48:03 -0500

    dict full

--- Udacity/17_Dictionaries/main.py
+++ Udacity/17_Dictionaries/main.py
@@ -2,11 +2,22 @@
 
 #mutable map from hashtable keys to arbitrary values
 
-empty = {}
-print(type(empty))
 
-a = {'one': 1, 'two': 2, 'three': 3}
-dict(one=1, two=2, three=3)
+# d = {"one": 1, "two": 2, "three": 3}
 
-print(a)
-print(dict)
+# print(d['one'])
+# # print(d['five']) #error
+
+# d['two'] = 22 #modify
+# d['four'] = 4  # Add a new key
+# print(d)
+
+
+#-----------------------
+temps = {'CA': [101, 115, 108], 'NY': [98, 102]}
+print(temps)
+print(temps.get('CA'))
+
+ky_temps = temps.get('KY', [])
+num_observations = len(ky_temps)
+print(num_observations)
