e7bfe1165833add2687b288c366cbf4965c06097
Jihun Park <jxxparker@gmail.com>
2021-06-29 11:37:32 -0500

    dict intro

--- Udacity/17_Dictionaries/main.py
+++ Udacity/17_Dictionaries/main.py
@@ -1,3 +1,12 @@
 #dictionary
 
 #mutable map from hashtable keys to arbitrary values
+
+empty = {}
+print(type(empty))
+
+a = {'one': 1, 'two': 2, 'three': 3}
+dict(one=1, two=2, three=3)
+
+print(a)
+print(dict)
